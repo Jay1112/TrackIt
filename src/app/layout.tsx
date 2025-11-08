6efc847cbdc9f17316f2cab048b8f68410b82ab2
Jay Patel <jaypatel@Jays-MacBook-Pro.local>
2025-11-08 19:08:41 +0530

    paths fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,9 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>
+        {/* <AuthProvider> */}
           {children}
-        </AuthProvider>
+        {/* </AuthProvider> */}
       </body>
     </html>
   );
